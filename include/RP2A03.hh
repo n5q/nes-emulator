7f8d56175ca9e045030ca5b4d4599ded83ccb171
n5q <nsq263@gmail.com>
2026-01-06 00:29:27 -0700

    a

---
+++ include/RP2A03.hh
@@ -0,0 +1,39 @@
+#ifndef RP2A03_HH
+#define RP2A03_HH
+
+#include <cstdint>
+#include <memory>
+
+class Bus;
+class PPU;
+
+class RP2A03 {
+public:
+  RP2A03();
+  ~RP2A03();
+
+  void connect_bus(std::shared_ptr<Bus> bus);
+  void connect_ppu(std::shared_ptr<PPU> ppu);
+
+  // 2A03 interprets writes to $4000 - $4017
+  void cpu_write(uint16_t addr, uint8_t data);
+  uint8_t cpu_read(uint16_t addr);
+
+  void clk();
+
+  // DMA
+  // DMA transfer suspends cpu for 513/514 cycles
+  bool dma_transfer = false;
+  bool dma_alignment = true;
+  uint8_t dma_page = 0x00;
+  uint8_t dma_addr = 0x00;
+  uint8_t dma_data = 0x00;
+
+private:
+  std::shared_ptr<Bus> bus;
+  std::shared_ptr<PPU> ppu;
+
+  // TODO: APU
+};
+
+#endif
